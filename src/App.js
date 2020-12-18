cccae508f873eac4ecfbb0ebbb431167ec4448e6
Andrew Nordman <cadwallion@gmail.com>
2020-12-17 16:38:02 -0800

    Initial question retrieval and display in frontend
    This implements QuestionList and Question components responsible for fetching the current round's questions from the API server and displaying them.  The fetching dynamically sets the endpoint based upon the round, so the QuestionList can have a different `round` property passed to it and have it change the endpoint being hit.  We do this because it decouples the QuestionList from any understanding of the context of what a Round is, and to be able to re-use the QuestionList for subsequent Round implementations.
    
    The UI is ugly, and that's by design.  Improving the UI will come next now that the functionality for display is complete.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { Box, Heading, Timeline } from '@primer/components';
 import './App.css';
+
+// The Question contains how to display a single question from a QuestionList.
+// While not strictly necessary that this be split out, I find this cleans up
+// the collection Component nicely. YMMV.
+const Question = (props) => {
+  const { number, question } = props;
+
+  return (
+    <Timeline.Item>
+      <Timeline.Badge>{number}</Timeline.Badge>
+      <Timeline.Body>{question}</Timeline.Body>
+    </Timeline.Item>
+  );
+};
+
+// The QuestionList contains the representation of Questions for a given Round.
+// We use the useEffect function hook to handle fetching the question data and
+// updating the state of the Component.  We do it this way because state changes
+// cause a re-render of the Component.  Once the fetch completes, we'll have data
+// to display.
+const QuestionList = (props) => {
+  const [listState, setListState] = useState({
+    round: props.round,
+    questions: null
+  })
+
+  useEffect(() => {
+    fetch("/round" + listState.round)
+      .then(res => res.json())
+      .then(res => setListState({ round: listState.round, questions: res.questions }))
+  }, [listState, setListState]);
+
+  const { questions, round } = listState;
+  if (!questions) return <div class="loading">Loading...</div>;
+
+  return (
+    <Box>
+      <Heading>Round {round}</Heading>
+      <Timeline>
+        {
+          questions.map((question) => <Question number={questions.indexOf(question) + 1} question={question} />)
+        }
+      </Timeline>
+    </Box>
+  )
+};
 
 function App() {
   fetch("/ping").then(res => res.text()).then(res => console.log(res));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <QuestionList round={1} />
+    </Box>
   );
 }
 
