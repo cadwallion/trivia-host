d5f341e9a7c4cc756fc013093db1ae2a6dab3ed2
Andrew Nordman <cadwallion@gmail.com>
2020-12-17 16:27:31 -0800

    Implement /round1 server endpoint
    This creates an endpoint at GET /round1 that returns a JSON object representing all the questions for the round.
    
    Why do we do this instead of coding the questions into the frontend?  The answer is separation of concerns to prepare for ease of refactoring.  Right now, this is a hardcoded array of questions, but a future refactor will turn this into a database-backed set of questions, so we can store the questions for each round and let the endpoint focus only on retrieval and handoff to the frontend for display.

--- server.js
+++ server.js
@@ -3,4 +3,21 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/ping', (req, res) => res.send("PONG"));
+app.get('/round1', (req, res) => {
+  const data = {
+    questions: [
+      "What is Question 1?",
+      "What is Question 2?",
+      "What is Question 3?",
+      "What is Question 4?",
+      "What is Question 5?",
+      "What is Question 6?",
+      "What is Question 7?",
+      "What is Question 8?",
+      "What is Question 9?",
+      "What is Question 10?"
+    ]
+  }
+  res.json(data)
+})
 app.listen(8080)
