4dee54053e37c6a5aac387c588473503402efb09
Andrew Nordman <cadwallion@github.com>
2021-04-21 10:34:36 -0700

    Add Game scaffold
    Co-authored-by: Brian Matejka <Brian-Demon@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_21_163441) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.datetime "played_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
