351d7c8e57e9e6471b548817556f020378632bf3
Brian-Demon <BrianWMatejka@gmail.com>
2021-04-23 14:47:03 -0500

    test helper methods for rounds_controller

--- test/controllers/rounds_controller_test.rb
+++ test/controllers/rounds_controller_test.rb
@@ -4,4 +4,40 @@ class RoundsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "activate method activates a round" do
+    @game = Game.create(name: "Test Game")
+    @round = @game.rounds.create(position: 0, category: "test", round_type: "question")
+    @round.save
+    refute @round.active
+    post activate_game_round_path(@game, @round.round_number)
+    assert @round.reload.active
+  end
+
+  test "deactivate method deactivates a round" do
+    @game = Game.create(name: "Test Game")
+    @round = @game.rounds.create(position: 0, active: true, category: "test", round_type: "question")
+    @round.save
+    assert @round.active
+    post deactivate_game_round_path(@game, @round.round_number)
+    refute @round.reload.active
+  end
+
+  test "complete method completes a round" do
+    @game = Game.create(name: "Test Game")
+    @round = @game.rounds.create(position: 0, category: "test", round_type: "question")
+    @round.save
+    refute @round.completed
+    post complete_game_round_path(@game, @round.round_number)
+    assert @round.reload.completed
+  end
+
+  test "continue method continues a round" do
+    @game = Game.create(name: "Test Game")
+    @round = @game.rounds.create(position: 0, completed: true, category: "test", round_type: "question")
+    @round.save
+    assert @round.completed
+    post continue_game_round_path(@game, @round.round_number)
+    refute @round.reload.completed
+  end
 end
