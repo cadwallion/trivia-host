1007e157adac45e2532ffc4f44082c70759f4656
Andrew Nordman <cadwallion@gmail.com>
2021-05-01 00:16:35 +0000

    Parse Audio and Final Rounds
    Co-authored-by: Brian Matejka <Brian-Demon@users.noreply.github.com>

--- app/models/trivia_importer.rb
+++ app/models/trivia_importer.rb
@@ -22,7 +22,11 @@ class TriviaImporter
     when /Picture Round/
       { category: "Picture Round", questions: parse_picture_round(data, page.xobjects) }
     when /Audio Round/
-      { category: "Audio Round", data: data }
+      { category: "Audio Round", questions: parse_audio_round(data) }
+    when /Final Trivia Question/
+      match = category.match(/Category: (?<category>.+)\n+Question: (?<question>.+):/m)
+      data.unshift(match["question"])
+      { category: match["category"].chomp, questions: parse_final_round(data) }
     else
       { category: category, questions: parse_question_round(data) }
     end
@@ -52,5 +56,30 @@ class TriviaImporter
         answer: answers[index]
       } 
     end
+  end
+
+  def parse_audio_round data
+    question_url_fragment = data.last.match(/https:\/\/quiznight.vids.io\/videos\/(.*)/)[1]
+    question_url = "https://quiznight.vids.io/videos/#{question_url_fragment}"
+    data.map do |answer|
+      match = answer.match(/“(?<title>.*)” – (?<artist>.+)/)
+      formatted_answer = "\"#{match["title"]}\" - #{match["artist"]}"
+      {
+        question: question_url,
+        answer: formatted_answer
+      }
+    end
+  end
+
+  def parse_final_round data
+    question = data.shift.gsub("\n", " ")
+    question_count =  data.length / 2
+    options = question_count.times.map do |index|
+      {
+        question: data[index].gsub("\n", "").gsub("Answer:", "").strip,
+        answer:  data[index+question_count].gsub("\n", "").strip
+      }
+    end
+    [{question: question, answer: "lolgetrekt" }] + options
   end
 end
